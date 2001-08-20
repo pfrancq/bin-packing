3d583d4d650c8fab457f0d20d234b58c6473c0b8
pfrancq <pfrancq@18ca93f4-cd2b-0410-8306-961d1ad44720>
2001-08-20 08:58:59 +0000

    - Windows to show Projects and Heuristics.

---
+++ kbinpacking/kbpprjview.cpp
@@ -0,0 +1,103 @@
+/*
+
+  kvlsiprjview.cpp
+
+  Description - Implementation.
+
+  (c) 2000 by P. Francq.
+
+  Version $Revision$
+
+  Last Modify: $Date$
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+
+
+//-----------------------------------------------------------------------------
+// include files for ANSI C/C++
+#include <stdio.h>
+
+
+//-----------------------------------------------------------------------------
+// include files for current application
+#include "kbpprjview.h"
+#include "kbinpackingdoc.h"
+
+
+
+//-----------------------------------------------------------------------------
+//
+// class KBPPrjView
+//
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+KBPPrjView::KBPPrjView(KBinPackingDoc* pDoc,QWidget *parent,const char *name,int wflags)
+	: KBinPackingView(pDoc,parent,name,wflags),prj(0)
+{
+	prj=new QListView(this,"Project Elements"+doc->URL().path());
+	prj->addColumn("Elements");
+	prj->addColumn("Sizes");
+	prj->setRootIsDecorated(true);
+	prj->setSorting(-1);
+	createPrj();
+}
+
+
+//-----------------------------------------------------------------------------
+void KBPPrjView::createPrj(void)
+{
+	char tmp[100];
+	char tmp2[10];
+	QListViewItem *item=0,*item2=0;
+	RBP::RObjBP **obj;
+	unsigned int i;
+	double total=0.0;
+
+	// Problem
+	sprintf(tmp,"%.1f",doc->MaxSize);
+	item=new QListViewItem(prj,"Problem",tmp);
+
+	// Construct Objects
+	sprintf(tmp,"Objects (%u)",doc->NbObjs);
+	item = new QListViewItem(prj,item,tmp,"");
+	item2=0;
+	for(obj=doc->Objs,i=doc->NbObjs;--i;obj++)
+	{
+		total+=(*obj)->GetSize();
+		sprintf(tmp,"%s (%u)",((*obj)->GetName())(),(*obj)->GetId());
+		sprintf(tmp2,"%.1f",(*obj)->GetSize());
+		item2 = new QListViewItem(item,item2,tmp,tmp2);
+	}
+	sprintf(tmp,"%.1f",total);
+	item->setText(1,tmp);
+	prj->setColumnWidth(0,prj->columnWidth(0)+20);
+}
+
+
+//-----------------------------------------------------------------------------
+void KBPPrjView::resizeEvent(QResizeEvent *)
+{
+	prj->resize(width(),height());
+}
+
+
+//-----------------------------------------------------------------------------
+KBPPrjView::~KBPPrjView()
+{
+}
---
+++ kbinpacking/kbpprjview.h
@@ -0,0 +1,100 @@
+/*
+
+  kbpprjview.h
+
+  Description - Header.
+
+  (c) 2000 by P. Francq.
+
+  Version $Revision$
+
+  Last Modify: $Date$
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+
+
+//-----------------------------------------------------------------------------
+#ifndef KBPPRJVIEW_H
+#define KBPPRJVIEW_H
+
+
+//-----------------------------------------------------------------------------
+// include files for Qt
+#include <qwidget.h>
+#include <qlistview.h>
+
+
+//-----------------------------------------------------------------------------
+// include files for current application
+#include "kbinpackingview.h"
+
+
+//-----------------------------------------------------------------------------
+/**
+* The KBPPrjView provides a representation of a project window.
+* @author Pascal Francq
+*/
+class KBPPrjView : public KBinPackingView
+{
+	Q_OBJECT
+
+	/**
+	* List representing the elements of the projects.
+	*/
+	QListView *prj;
+
+public:
+
+	/**
+	* Construct the project view.
+	*/
+	KBPPrjView(KBinPackingDoc* pDoc,QWidget *parent, const char *name,int wflags);
+
+	/**
+	* Return the type of the window.
+	*/
+	virtual BPViewType getType(void) {return(Project);}
+
+	/**
+	* Ask for setting the title.
+	*/
+	virtual void setTitle(QString _title) {setCaption("Project: "+_title);}
+
+	/**
+	* Construct the project tree.
+	*/
+	void createPrj(void);
+
+protected:
+
+	/**
+	* Handle the resize of this window.
+	*/
+	void resizeEvent(QResizeEvent *);
+
+public:
+
+	/**
+	* Destruct the project view.
+	*/
+	~KBPPrjView();
+};
+
+
+//-----------------------------------------------------------------------------
+#endif
