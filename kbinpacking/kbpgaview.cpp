f94fa3523dd89352cf24ea10a0f6ea7fb1a5ef99
pfrancq <pfrancq@18ca93f4-cd2b-0410-8306-961d1ad44720>
2001-08-24 16:28:04 +0000

    - Windows to track Bin Packing Problems.

---
+++ kbinpacking/kbpgaview.cpp
@@ -0,0 +1,253 @@
+/*
+
+  KBPGAView.cpp
+
+  Description - Implementation.
+
+  (c) 2001 by P. Francq.
+
+  Version $Revision$
+
+  Last Modify: $Date$
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+
+
+//-----------------------------------------------------------------------------
+// include files for R Project
+#include <rbp/rfinstbp.h>
+#include <rbp/rfchromobp.h>
+using namespace RBP;
+
+
+//-----------------------------------------------------------------------------
+// include files for Qt/KDE
+#include <klocale.h>
+#include <kmessagebox.h>
+
+
+//-----------------------------------------------------------------------------
+// include files for Widgets
+#include <rga/qgamonitor.h>
+#include <rga/qxmlcontainer.h>
+using namespace RGA;
+#include <rbp/qdrawgroups.h>
+using namespace RBP;
+
+
+//-----------------------------------------------------------------------------
+// include files for current application
+#include "kbpgaview.h"
+#include "kbinpackingdoc.h"
+
+
+//-----------------------------------------------------------------------------
+//
+// class KBPGAView
+//
+//-----------------------------------------------------------------------------
+
+//-----------------------------------------------------------------------------
+KBPGAView::KBPGAView(KBinPackingDoc* pDoc,QWidget *parent, const char *name,int wflags)
+	: KBinPackingView(pDoc,parent,name,wflags), CurId(0), Instance(0), Data(pDoc->MaxSize)
+{
+	static char tmp[100];
+	setSizePolicy( QSizePolicy( (QSizePolicy::SizeType)1, (QSizePolicy::SizeType)1, sizePolicy().hasHeightForWidth() ) );
+	TabWidget = new QTabWidget( this, "TabWidget" );
+	TabWidget->setGeometry(rect());
+	TabWidget->setSizePolicy( QSizePolicy( (QSizePolicy::SizeType)7, (QSizePolicy::SizeType)7, TabWidget->sizePolicy().hasHeightForWidth() ) );
+	TabWidget->setBackgroundOrigin( QTabWidget::ParentOrigin );
+
+	// Stat part
+	StatSplitter=new QSplitter(QSplitter::Vertical,TabWidget,"Statistic");
+	TabWidget->insertTab(StatSplitter,"Statistic");
+	StatSplitter->setGeometry(rect());
+	Monitor=new	QGAMonitor(StatSplitter);
+	Monitor->setMaxGen(theApp->GAMaxGen);
+	Monitor->setMaxFitness(pDoc->NbObjs/2);
+	connect(this,SIGNAL(signalSetGen(const unsigned int,const unsigned int,const double)),Monitor,SLOT(slotSetGen(const unsigned int,const unsigned int,const double)));
+	Debug=new QXMLContainer(StatSplitter);
+
+	// Solution part
+	Best = new QDrawGroups(TabWidget,pDoc->Objs);
+	TabWidget->insertTab(Best,"Best Solution");
+
+	// Solution part
+	Sol = new QDrawGroups(TabWidget,pDoc->Objs);
+	sprintf(tmp,"Solution (0/%u)",((KBinPackingApp*)parentWidget()->parentWidget()->parentWidget())->GAPopSize-1);
+	TabWidget->insertTab(Sol,tmp);
+
+	// Create GA
+	try
+	{
+		Gen=0;
+		Instance=new RFInstBP(theApp->GAMaxGen,theApp->GAPopSize,pDoc->Objs,pDoc->NbObjs,theApp->GAHeur,pDoc->MaxSize,Debug);
+		Instance->AddReceiver(this);
+		Instance->Init(&Data);
+	}
+	catch(eGA& e)
+	{
+		KMessageBox::error(this,QString(e.Msg));
+		Instance=0;
+	}
+	catch(bad_alloc)
+	{
+		KMessageBox::error(this,"Memory Problems");
+		Instance=0;
+	}
+	catch(...)
+	{
+		KMessageBox::error(this,"Unknow Problem");
+		Instance=0;
+	}
+}
+
+
+//---------------------------------------------------------------------------
+void KBPGAView::receiveGenSig(GenSig* sig)
+{
+	emit signalSetGen(sig->Gen,sig->BestGen,sig->Best->Used.NbPtr/*Fitness->Value*/);
+	Sol->setGroups(Instance->Chromosomes[CurId]);
+	Sol->setChanged();
+}
+
+
+//---------------------------------------------------------------------------
+void KBPGAView::receiveInteractSig(InteractSig* /*sig*/)
+{
+	KApplication::kApplication()->processEvents(1000);
+}
+
+
+////---------------------------------------------------------------------------
+void KBPGAView::receiveBestSig(BestSig* sig)
+{
+	static char tmp[100];
+
+	sprintf(tmp,"Best Solution (Id=%u)",sig->Best->Id);
+	TabWidget->changeTab(Best,tmp);
+	Best->setGroups(sig->Best);
+	Best->setChanged();
+}
+
+
+//---------------------------------------------------------------------------
+void KBPGAView::RunGA(void)
+{
+	if(Instance)
+	{
+		try
+		{		
+			if(theApp->GAMaxGen>Gen)
+			{
+				if(theApp->GAStepGen==0)
+					Gen=theApp->GAMaxGen;
+				else
+				{
+					Gen+=theApp->GAStepGen;
+					if(Gen>theApp->GAMaxGen) Gen=theApp->GAMaxGen;
+				}
+			}
+			Instance->MaxGen=Gen;
+			Instance->Run();
+			if(Gen==theApp->GAMaxGen)
+				theApp->GAPause->setEnabled(false);
+			KMessageBox::information(this,"Done");
+		}
+		catch(eGA& e)
+		{
+			KMessageBox::error(this,QString(e.Msg));
+		}
+	}
+}
+
+
+//---------------------------------------------------------------------------
+void KBPGAView::PauseGA(void)
+{
+	ExternBreak=true;
+}
+
+
+//---------------------------------------------------------------------------
+void KBPGAView::StopGA(void)
+{
+}
+
+
+//---------------------------------------------------------------------------
+void KBPGAView::keyReleaseEvent(QKeyEvent* e)
+{
+	static char tmp[100];
+//	QGoToPopDlg *dlg;
+
+	if(TabWidget->currentPage()!=Sol)
+	{
+		KBinPackingView::keyReleaseEvent(e);
+		return;
+	}
+	switch(e->key())
+	{
+		case Key_PageUp:
+			if(CurId<Instance->PopSize-1) CurId++; else CurId=0;
+			sprintf(tmp,"Solution (%u/%u)",CurId,Instance->PopSize-1);
+			TabWidget->changeTab(Sol,tmp);
+			Sol->setGroups(Instance->Chromosomes[CurId]);
+			Sol->setChanged();
+			break;
+
+		case Key_PageDown:
+			if(CurId>0) CurId--; else CurId=Instance->PopSize-1;
+			sprintf(tmp,"Solution (%u/%u)",CurId,Instance->PopSize-1);
+			TabWidget->changeTab(Sol,tmp);
+			Sol->setGroups(Instance->Chromosomes[CurId]);
+			Sol->setChanged();
+			break;
+
+//		case Key_G:
+//			if(e->state()==ControlButton)
+//			{
+//				dlg= new QGoToPopDlg(Doc->getPopSize());
+//				if(dlg->exec())
+//				{
+//					CurId=dlg->PopIndex->value();
+//					slotNewChromo();
+//				}
+//				delete dlg;
+//			}
+//			break;
+
+		default:
+			e->ignore();
+	}
+}
+
+
+//-----------------------------------------------------------------------------
+void KBPGAView::resizeEvent(QResizeEvent*)
+{
+	TabWidget->resize(size());
+}
+
+
+//-----------------------------------------------------------------------------
+KBPGAView::~KBPGAView()
+{
+	if(Instance)
+		delete Instance;
+}
---
+++ kbinpacking/kbpgaview.h
@@ -0,0 +1,210 @@
+/*
+
+  kvlsigaview.h
+
+  Description - Header.
+
+  (c) 2001 by P. Francq.
+
+  Version $Revision$
+
+  Last Modify: $Date$
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+
+*/
+
+
+
+//-----------------------------------------------------------------------------
+#ifndef KVLSIGAVIEW_H
+#define KVLSIGAVIEW_H
+
+
+//-----------------------------------------------------------------------------
+// include files for R Project
+#include <rga/rgasignals.h>
+using namespace RGA;
+#include <rbp/rdatabpfile.h>
+using namespace RBP;
+
+
+//-----------------------------------------------------------------------------
+// include files for Qt
+#include <qwidget.h>
+#include <qsplitter.h>
+#include <qtabwidget.h>
+
+
+//-----------------------------------------------------------------------------
+// include files for current application
+#include "kbinpackingview.h"
+
+
+//-----------------------------------------------------------------------------
+// forward declaration
+namespace RGA
+{
+	class QGAMonitor;
+	class QXMLContainer;
+}
+using namespace RBP;
+namespace RBP
+{
+	class QDrawGroups;
+}
+using namespace RBP;
+
+
+//-----------------------------------------------------------------------------
+/**
+* The KVLSIGAView class provides a representation of a result of a GA that is
+* running.
+* @author Pascal Francq
+*/
+class KBPGAView : public KBinPackingView, public RGASignalsReceiver<RFInstBP,RFChromoBP,RFitnessBP>
+{
+	Q_OBJECT
+
+	/**
+	* The tab widget.
+	*/
+	QTabWidget* TabWidget;
+	
+	/**
+	* The widget that handle statistics about the running GA.	
+	*/
+	QGAMonitor* Monitor;
+	
+	/**
+	* The widget that handle the debug information of the running GA.
+	*/
+	QXMLContainer* Debug;
+	
+	/**
+	* The splitter that separate the monitor and the debug part.
+	*/
+	QSplitter* StatSplitter;
+	
+	/**
+	* The best solution.
+	*/
+	QDrawGroups* Best;
+	
+	/**
+	* The solutions.
+	*/
+	QDrawGroups* Sol;
+	
+	/**
+	* Identificator of the current showed solution.
+	*/
+	unsigned int CurId;
+
+	/**
+	* The GA that will be used.
+	*/
+	RFInstBP* Instance;
+
+	/**
+	* Number of generation already executed.
+	*/
+	unsigned int Gen;
+
+	/**
+	* Data needed for the construction of the groups.
+	*/
+	RGroupDataBP Data;
+
+public:
+
+	/**
+	* Constructor for the view.
+	* @param pDoc           your document instance that the view represents. Create a
+	*                       document before calling the constructor or connect an
+	*                       already existing document to a new MDI child widget.
+	*/
+	KBPGAView(KBinPackingDoc* pDoc,QWidget *parent, const char *name,int wflags);
+
+	/**
+	* Return the type of the window.
+	*/
+	virtual BPViewType getType(void) {return(GA);}
+
+	/**
+	* GA signal to indicate that a new generation has been done.
+	*/
+	virtual void receiveGenSig(GenSig* sig);
+	
+	/**
+	* GA signal to interact with the system.
+	*/
+	virtual void receiveInteractSig(InteractSig* sig);
+
+	/**
+	* GA signal to signify that the best chromosome has changed.
+	*/
+	virtual void receiveBestSig(BestSig* sig);
+
+	/**
+	* Run the GA.
+	*/
+	void RunGA(void);
+
+	/**
+	* Pause the GA.
+	*/
+	void PauseGA(void);
+
+	/**
+	* Stop the GA.
+	*/
+	void StopGA(void);
+
+protected:
+
+	/**
+	* Key release event method. The implementation is needed to change the
+	* chromosome to show when the user uses the keys "Page Up" and "Page Down".
+	* With "Ctrl-G", a dialog box appears to select directly the identificator.
+	*/
+	virtual void keyReleaseEvent(QKeyEvent* e);
+
+	/**
+	* The function that handle the resize event.
+	*/
+	virtual void resizeEvent(QResizeEvent*);
+
+signals:
+
+	/**
+	* Signal to emit after a generation is done.
+	*/
+	void signalSetGen(const unsigned int gen,const unsigned int best,const double value);
+
+public:
+
+	/**
+	* Destruct the view.
+	*/
+	~KBPGAView();
+
+	// friend classes
+	friend class KBinPackingDoc;
+};
+
+
+//-----------------------------------------------------------------------------
+#endif
